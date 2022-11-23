b0000c5bfa2421791f4d31bf3a25b70b91a3f553
officialheartwell <officialheartwell@gmail.com>
2022-11-23 22:25:16 +0100

    all files have been checked and ready for upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("titleSection").style.marginTop = "150%";
+}
+function closeMenu() {
+  document.getElementById("sidebar").style.width = "0px";
+  document.getElementById("titleSection").style.marginTop = "0px";
+}
